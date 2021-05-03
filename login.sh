d82e0a16d9864a1910131e05b8d80068f5954315
Martín Jiménez <jimenez.martin.jam@gmail.com>
2021-05-03 14:23:37 -0500

    login

---
+++ login.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$PAM_TYPE" != "close_session" ]; then
+        url_slack=<webhook>
+        url_telegram=<https://api.telegram.org/bot<token>/sendmessage>
+        chat_id=<#>
+        channel=<n>
+        host="$(hostname)"
+        content="\"attachments\": [ { \"mrkdwn_in\": [\"text\", \"fallback\"], \"fallback\": \"SSH login: $PAM_USER connected to \`$host\`\", \"text\": \"SSH login to \`$host\`\", \"fields\": [ { \"title\": \"User\", \"valu>        text="SSH login: <b>$PAM_USER</b> on <b>$host</b> <b>$PAM_RHOST</b>"
+        curl -X POST --data-urlencode "payload={\"channel\": \"$channel\", \"mrkdwn\": true, \"username\": \"SSH Login\", $content}" "$url_slack" &
+        curl --location --request POST "$url_telegram?chat_id=$chat_id&text=$text&parse_mode=html" &
+fi
+exit
