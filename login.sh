e866c65738a58ab2a57daae8aed9ca9f7b62523a
Martín Jiménez <jimenez.martin.jam@gmail.com>
2021-05-03 14:28:51 -0500

     

--- login.sh
+++ login.sh
@@ -6,7 +6,8 @@ if [ "$PAM_TYPE" != "close_session" ]; then
         chat_id=<#>
         channel=<n>
         host="$(hostname)"
-        content="\"attachments\": [ { \"mrkdwn_in\": [\"text\", \"fallback\"], \"fallback\": \"SSH login: $PAM_USER connected to \`$host\`\", \"text\": \"SSH login to \`$host\`\", \"fields\": [ { \"title\": \"User\", \"valu>        text="SSH login: <b>$PAM_USER</b> on <b>$host</b> <b>$PAM_RHOST</b>"
+        content="\"attachments\": [ { \"mrkdwn_in\": [\"text\", \"fallback\"], \"fallback\": \"SSH login: $PAM_USER connected to \`$host\`\", \"text\": \"SSH login to \`$host\`\", \"fields\": [ { \"title\": \"User\", \"value\": \"$PAM_USER\", \"short\": true }, { \"title\": \"IP Address\", \"value\": \"$PAM_RHOST\", \"short\": true } ], \"color\": \"#F35A00\" } ]"
+		text="SSH login: <b>$PAM_USER</b> on <b>$host</b> <b>$PAM_RHOST</b>"
         curl -X POST --data-urlencode "payload={\"channel\": \"$channel\", \"mrkdwn\": true, \"username\": \"SSH Login\", $content}" "$url_slack" &
         curl --location --request POST "$url_telegram?chat_id=$chat_id&text=$text&parse_mode=html" &
 fi
